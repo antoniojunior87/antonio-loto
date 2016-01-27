7a1caf94f590ee0827688cb4e7e036f8420eb0e0
Antonio <antonio.junior@uniriotec.br>
2016-01-26 22:25:41 -0200

    ajustes

--- loto/src/main/java/com/antonio/loto/arquivo/Leitor.java
+++ loto/src/main/java/com/antonio/loto/arquivo/Leitor.java
@@ -1,5 +1,20 @@
 package com.antonio.loto.arquivo;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
 import com.antonio.loto.Resultado;
 import com.antonio.loto.arquivo.html.Body;
 import com.antonio.loto.arquivo.html.Tr;
@@ -7,121 +22,96 @@ import com.thoughtworks.xstream.XStream;
 import com.thoughtworks.xstream.io.xml.DomDriver;
 import com.thoughtworks.xstream.mapper.CannotResolveClassException;
 import com.thoughtworks.xstream.mapper.MapperWrapper;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
-import org.apache.commons.io.FileUtils;
 
 /**
  * @author Junior
  */
 public class Leitor {
 
-    public static Body ler(InputStream input) throws FileNotFoundException, IOException {
+	public static Body ler(InputStream input) throws FileNotFoundException, IOException {
 
-        File arq = new File("src/main/resources/tempfile");
-        OutputStream out = new FileOutputStream(arq);
-        byte buf[] = new byte[1024];
-        int len;
-        while ((len = input.read(buf)) > 0) {
-            out.write(buf, 0, len);
-        }
-        out.close();
-        input.close();
+		File arq = File.createTempFile("resultados", "html");
+		OutputStream out = new FileOutputStream(arq);
+		byte buf[] = new byte[1024];
+		int len;
+		while ((len = input.read(buf)) > 0) {
+			out.write(buf, 0, len);
+		}
+		out.close();
+		input.close();
 
-        XStream xStream = new XStream(new DomDriver()) {
-            @Override
-            protected MapperWrapper wrapMapper(MapperWrapper next) {
-                return new MapperWrapper(next) {
-                    @Override
-                    public boolean shouldSerializeMember(Class definedIn, String fieldName) {
-                        try {
-                            return definedIn != Object.class || realClass(fieldName) != null;
-                        } catch (CannotResolveClassException cnrce) {
-                            return false;
-                        }
-                    }
-                };
-            }
-        };
-        xStream.alias("body", Body.class);
-        xStream.alias("tr", Tr.class);
-        xStream.alias("td", String.class);
-        xStream.addImplicitCollection(Body.class, "linhas");
-        xStream.addImplicitCollection(Tr.class, "colunas");
+		XStream xStream = new XStream(new DomDriver()) {
+			@Override
+			protected MapperWrapper wrapMapper(MapperWrapper next) {
+				return new MapperWrapper(next) {
+					@Override
+					public boolean shouldSerializeMember(Class definedIn, String fieldName) {
+						try {
+							return definedIn != Object.class || realClass(fieldName) != null;
+						} catch (CannotResolveClassException cnrce) {
+							return false;
+						}
+					}
+				};
+			}
+		};
+		xStream.alias("body", Body.class);
+		xStream.alias("tr", Tr.class);
+		xStream.alias("td", String.class);
+		xStream.addImplicitCollection(Body.class, "linhas");
+		xStream.addImplicitCollection(Tr.class, "colunas");
 
-        String str = FileUtils.readFileToString(arq);
-        str = str.replaceAll(" bgcolor=#D9E6F4", "");
-        int primeiro = str.indexOf("</tr>");
-        int utimo = str.lastIndexOf("</tr>");
+		String str = FileUtils.readFileToString(arq);
+		str = str.replaceAll(" bgcolor=#D9E6F4", "");
+		str = str.replaceAll("&nbsp", "");
+		int primeiro = str.indexOf("</tr>");
+		int utimo = str.lastIndexOf("</tr>");
 
-        StringBuilder builder = new StringBuilder();
-        builder.append("<body>");
-        builder.append(str.substring(primeiro + 5, utimo));
-        builder.append("</tr>");
-        builder.append("</body>");
-        return (Body) xStream.fromXML(builder.toString());
-    }
+		StringBuilder builder = new StringBuilder();
+		builder.append("<body>");
+		builder.append(str.substring(primeiro + 5, utimo));
+		builder.append("</tr>");
+		builder.append("</body>");
+		return (Body) xStream.fromXML(builder.toString());
+	}
 
-    public static File gravaArquivoDeURL(String stringUrl, String pathLocal) {
-        try {
-            System.setProperty("http.maxRedirects", "40");
-            //Encapsula a URL num objeto java.net.URL
-            URL url = new URL(stringUrl);
-//            HttpURLConnection.setFollowRedirects(false);
-            HttpURLConnection connection = null;
-            connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestProperty("User-Agent", "Mozilla/5.0 (X11; Linux i686) AppleWebKit/534.30 (KHTML, like Gecko) Ubuntu/11.04 Chromium/12.0.742.112 Chrome/12.0.742.112 Safari/534.30");
-            try {
-                /*System.out.println("Response code = " + */ connection.getResponseCode();//);
-            } catch (java.net.ProtocolException ex) {
-                throw new Exception("Não foi possivel baixar o arquivo");
-            }
+	public static File gravaArquivoDeURL(String stringUrl) {
+		try {
 
-            //Queremos o arquivo local com o mesmo nome descrito na URL
-            //Lembrando que o URL.getPath() ira retornar a estrutura 
-            //completa de diretorios e voce deve tratar esta String
-            //caso nao deseje preservar esta estrutura no seu disco local.
-            String nomeArquivoLocal = url.getPath();
-            if (!nomeArquivoLocal.isEmpty()) {
-                String[] path = nomeArquivoLocal.trim().split("/");
-                nomeArquivoLocal = path[path.length - 1];
-            }
-            //Cria streams de leitura (este metodo ja faz a conexao)...
-            InputStream is = url.openStream();
-            //... e de escrita.
-            FileOutputStream fos = new FileOutputStream(pathLocal + nomeArquivoLocal);
-            //Le e grava byte a byte. Voce pode (e deve) usar buffers para
-            //melhor performance (BufferedReader).
-            int umByte = 0;
-            while ((umByte = is.read()) != -1) {
-                fos.write(umByte);
-            }
-            //Nao se esqueca de sempre fechar as streams apos seu uso!
-            is.close();
-            fos.close();
-            //apos criar o arquivo fisico, retorna referencia para o mesmo
-            return new File(pathLocal + nomeArquivoLocal);
-        } catch (Exception e) {
-            //Lembre-se de tratar bem suas excecoes, ou elas tambem lhe tratarão mal!
-            //Aqui so vamos mostrar o stack no stderr.
-            e.printStackTrace();
-        }
-        return null;
-    }
+			HttpGet httpget = new HttpGet(stringUrl);
+			CloseableHttpClient httpclient = HttpClients.createDefault();
+			CloseableHttpResponse response = httpclient.execute(httpget);
+			InputStream is = response.getEntity().getContent();
 
-    public static List<Resultado> obterListaResultado(Body resultado) {
-        List<Resultado> retorno = new ArrayList<Resultado>();
-        for (Tr tr : resultado.getLinhas()) {
-            retorno.add(new Resultado(tr.getColunas().toArray()));
-        }
-        return retorno;
-    }
+			File saida = File.createTempFile("mega", "sena");
+			saida.createNewFile();
+			FileOutputStream fos = new FileOutputStream(saida);
+
+			int umByte = 0;
+			while ((umByte = is.read()) != -1) {
+				fos.write(umByte);
+			}
+
+			// Nao se esqueca de sempre fechar as streams apos seu uso!
+			is.close();
+			fos.close();
+			// apos criar o arquivo fisico, retorna referencia para o mesmo
+			return saida;
+		} catch (Exception e) {
+			// Lembre-se de tratar bem suas excecoes, ou elas tambem lhe
+			// tratarão mal!
+			// Aqui so vamos mostrar o stack no stderr.
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public static List<Resultado> obterListaResultado(Body resultado) {
+		List<Resultado> retorno = new ArrayList<Resultado>();
+		for (Tr tr : resultado.getLinhas()) {
+			if (tr.getColunas().size() == 21)
+				retorno.add(new Resultado(tr.getColunas().toArray()));
+		}
+		return retorno;
+	}
 }
--- loto/src/test/java/com/antonio/loto/LeitorArquivoTest.java
+++ loto/src/test/java/com/antonio/loto/LeitorArquivoTest.java
@@ -1,12 +1,14 @@
 package com.antonio.loto;
 
-import com.antonio.loto.arquivo.Leitor;
-import com.antonio.loto.arquivo.html.Body;
 import java.io.InputStream;
 import java.util.Enumeration;
 import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
+
+import com.antonio.loto.arquivo.Leitor;
+import com.antonio.loto.arquivo.html.Body;
+
 import junit.framework.TestCase;
 
 /**
@@ -14,35 +16,38 @@ import junit.framework.TestCase;
  */
 public class LeitorArquivoTest extends TestCase {
 
-    public void testApp() {
-        try {
-            ZipFile zip = new ZipFile(Leitor.gravaArquivoDeURL("http://www1.caixa.gov.br/loterias/_arquivos/loterias/D_megase.zip", "src/main/resources/"));
-            assertNotNull(zip);
-            assertTrue(zip.size() > 0);
-            Enumeration e = zip.entries();
-            Body resultado = null;
-            List<Resultado> retorno = null;
-            while (e.hasMoreElements()) {
-                ZipEntry arq = (ZipEntry) e.nextElement();
-                if (arq.getName().endsWith(".HTM")) {
-                    try {
-                        InputStream in = zip.getInputStream(arq);
-                        resultado = Leitor.ler(in);
-                        retorno = Leitor.obterListaResultado(resultado);
-                        for (Resultado result : retorno) {
-                            System.out.println(result.toString());
-                        }
-                    } finally {
-                        zip.close();
-                    }
-                    break;
-                }
-            }
-            assertNotNull(resultado);
-            assertNotNull(retorno);
-            assertTrue(retorno.size() > 0);
-        } catch (Exception ex) {
-            fail();
-        }
-    }
+	public void testApp() {
+
+		try {
+			ZipFile zip = new ZipFile(
+					Leitor.gravaArquivoDeURL("http://www1.caixa.gov.br/loterias/_arquivos/loterias/D_megase.zip"));
+			assertNotNull(zip);
+			assertTrue(zip.size() > 0);
+			Enumeration e = zip.entries();
+			Body resultado = null;
+			List<Resultado> retorno = null;
+			while (e.hasMoreElements()) {
+				ZipEntry arq = (ZipEntry) e.nextElement();
+				if (arq.getName().endsWith(".HTM")) {
+					try {
+						InputStream in = zip.getInputStream(arq);
+						resultado = Leitor.ler(in);
+						retorno = Leitor.obterListaResultado(resultado);
+						for (Resultado result : retorno) {
+							System.out.println(result.toString());
+						}
+					} finally {
+						zip.close();
+					}
+					break;
+				}
+			}
+			assertNotNull(resultado);
+			assertNotNull(retorno);
+			assertTrue(retorno.size() > 0);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			fail();
+		}
+	}
 }
